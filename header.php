fd12dc4a232fc5562e601772192c4ae1b62ef869
Bruno <bruunohenrique@hotmail.com>
2021-02-08 20:33:40 -0300

    primeirinho

---
+++ header.php
@@ -0,0 +1,13 @@
+<div class="global_header d-flex justify-content-start align-items-center azul_escuro-marca-bg">
+    <div class="div_header-elementos">
+
+        <div class="div_header-logo">
+            <img class="logo-svg" src="img/logo-branco.svg" alt="">
+        </div> <!--Fim div_header-logo-->
+
+        <div class="header_botao">
+            <a href="#">Entrar /  Cadastrar-se</a>
+        </div>
+
+    </div> <!--Fim div_header-elementos-->
+</div> <!-- Fim global_header -->
