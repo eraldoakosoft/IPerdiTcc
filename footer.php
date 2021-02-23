84fd252597586df6dfabf3101c63c90ae66bd3ac
bruno <bruno@aigen.com.br>
2021-02-22 23:57:08 -0300

    Responsivo da página index finalizado

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
         
             <div class="footer_marca">
                 <p>iPerdi © 2021</p>
-                <p>Desenvolvido e mantido por <span>Acko Software</span></p>
+                <p class="p_acko-software">Desenvolvido e mantido por <span>Acko Software</span></p>
             </div>
         </div>
     </footer>
